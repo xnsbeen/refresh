50bd3a52235aa79983ae5e8cf52cb580f4191272
xnsbeen <ahohoiey@naver.com>
2020-12-24 20:46:18 +0900

    add button

---
+++ popup.js
@@ -0,0 +1,11 @@
+
+
+function refresh(){
+    document.querySelector("#aa4231").;
+}
+
+
+document.addEventListener('DOMcontentloaded',function(){
+    var btn_start = document.querySelector('#btn_start');
+    btn_start.document.addEventListener('click',refresh);
+});
